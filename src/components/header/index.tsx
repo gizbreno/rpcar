8554ba2364cc35be9a1b3f9456baea5da3fb3ecb
gizbreno <brenojsilva@proton.me>
2025-03-07 15:09:02 -0300

    header personalize and config taiwlwind

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logot.png";
+import Logo from "../../assets/logo.png";
 import {FiUser,FiLogIn} from 'react-icons/fi'
 
 export const Header = () => {
   const signed = false;
   const loadingAuth = false;
   return (
-    <div  className="w-full flex">
-      <header>
+    <div  className="flex w-full items-center justify-center h-16 bg-white drop-shadow-sm mb-4">
+      <header className="w-full flex justify-between max-w-7xl items-center px-4 mx-auto">
         <Link to="/">
-          <img src={Logo} />
+          <img src={Logo} width={100} className="rounded-lg" />
         </Link>
+        {!loadingAuth && signed && 
+        
         <Link to="/dashboard">
-        <FiUser size={23}/></Link>
+        <div className="p-2 border-2 rounded-full border-gray-800"><FiUser size={22}/></div></Link>
+        }
+        {!loadingAuth && !signed && 
+        <Link to="/login">
+        <div className="p-2 border-2 rounded-full border-gray-800"><FiLogIn size={22}/></div></Link>}
       </header>
     </div>
   );

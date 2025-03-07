baa16ee50de001ed32f2398e8eaf52a2b9d262da
gizbreno <brenojsilva@proton.me>
2025-03-07 11:47:42 -0300

    configure header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logot.png";
+import {FiUser,FiLogIn} from 'react-icons/fi'
+
 export const Header = () => {
-    return (
-      <div><h1>Coponente header</h1></div>
-    )
-  }
-  
+  const signed = false;
+  const loadingAuth = false;
+  return (
+    <div  className="w-full flex">
+      <header>
+        <Link to="/">
+          <img src={Logo} />
+        </Link>
+        <Link to="/dashboard">
+        <FiUser size={23}/></Link>
+      </header>
+    </div>
+  );
+};
